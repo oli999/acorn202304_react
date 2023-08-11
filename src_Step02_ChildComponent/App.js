b9596caea9c5148d6d33363144fef4d0290c1dfb
kimgura <oli999@naver.com>
2023-08-11 17:23:18 +0900

    Step01, Step02 예제 추가함

--- src_Step02_ChildComponent/App.js
+++ src_Step02_ChildComponent/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import Child from './components/ChildComponent';
+import Contact from './components/ContactComponent';
+import MyName from './components/MyNameComponent';
 
 /*
 function App() {
@@ -18,10 +21,18 @@ class App extends Component{
   
   //render() 메소드 에서 리턴해주는 jsx 를 활용해서 화면 구성이 된다.
   render(){
+    const name="원숭이"
     return (
       <div className="container">
         <h1>인덱스 페이지 입니다.</h1>
-
+        <Child></Child>
+        <Child></Child>
+        <Child></Child>
+        <Contact></Contact>
+        <h3>MyNameComponent 사용해 보기</h3>
+        <MyName name={"김구라"}/>
+        <MyName name={"해골"}/>
+        <MyName name={name}/>
       </div>
     )
   }
