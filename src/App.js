a10c6f43fcdedf10f1baa01aaa513cd5339eebc9
kimgura <oli999@naver.com>
2023-09-01 09:57:12 +0900

    final 에 App.js 구성함

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
 
-/*
+import {useState, useRef} from 'react';
+
 function App() {
+
+  const noticeList=[
+    "React 수업 중입니다.",
+    "Built test 중...",
+    "어쩌구... 저쩌구..."
+  ];
+
+  const [notice, setNotice] = useState(noticeList);
+  const inputNotice=useRef();
+  //추가 버튼을 누르면 호출되는 함수
+  const handleAdd=()=>{ 
+    //input 요소에 입력한 내용을 읽어와서 state 변경 
+    setNotice([...notice, inputNotice.current.value]);
+    inputNotice.current.value="";
+  }
   return (
     <div className="container">
       <h1>인덱스 페이지 입니다.</h1>
-
+      <h3>공지사항</h3>
+      <div className="row">
+        <div className="col">
+          <input className="form-control" ref={inputNotice} type="text" placeholder='공지사항 입력...'/>
+        </div>
+        <div className="col">
+          <button className="btn btn-primary" onClick={handleAdd}>추가</button>
+        </div>
+      </div>
+      <ul>
+          {notice.map((item,index)=><li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
-*/
 
-//함수형 component 에서 클래스형 component 로 변경하기
-class App extends Component{
-  
-  //render() 메소드 에서 리턴해주는 jsx 를 활용해서 화면 구성이 된다.
-  render(){
-    return (
-      <div className="container">
-        <h1>인덱스 페이지 입니다.</h1>
-
-      </div>
-    )
-  }
-}
 
 
 // import 한 곳에 무엇을 리턴해줄지 결정하기 
