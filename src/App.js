f8bd2208cfa2a12d66b99f565917d23b919c2917
kimgura <oli999@naver.com>
2023-09-06 14:26:56 +0900

    토큰발급및 로그인테스중...

--- src/App.js
+++ src/App.js
@@ -2,8 +2,41 @@ import { NavLink, Route, Routes } from "react-router-dom";
 
 import {Home, Member, MemberForm, MemberUpdateForm} from './pages';
 import BsNavbar from "./components/BsNavbar";
+// jsontokens 에서 디코더 import
+import {decodeToken} from 'jsontokens';
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(()=>{
+      //localStorage 에 저장된 토큰을 디코딩 
+    const result=decodeToken(localStorage.token);
+
+    console.log(result);
+
+    //초단위
+    const expTime=result.payload.exp*1000; // *1000 을 해서 ms 단위로 만들고 
+    
+    //현재시간
+    const now=new Date().getTime();
+
+    if(expTime < now){
+      console.log("토큰 유효기간이 만료 되었습니다.");
+      //로그인 창을 띄운다.
+      //const userName=prompt("아이디 입력");
+      //const password=prompt("비밀번호 입력");
+      axios.post("http://localhost:9000/boot08/auth", 
+        {userName:"kimgura", password:"1234"}, {headers:{Authorization:"none"}})
+      .then(res=>{
+        alert("로그인 되었습니다.");
+        localStorage.token=res.data;
+      })
+      .catch(error=>{
+        console.log(error);
+      });
+    }
+  }, []);
 
   return (
     <>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 // src/pages/Home.js
 
 import {useState, useRef, useEffect} from 'react';
+import initToken from '../initToken';
 import axios from 'axios';
 axios.defaults.baseURL="http://localhost:9000/boot08";
 
 export default function Home(){
+    //localStorage 에 저장된 토큰을 사용할 준비를 한다.
+    initToken(localStorage.token);
+
     const [notice, setNotice] = useState([]);
 
     useEffect(()=>{
+      
       //공지 사항을 요청을 통해 받아온다.
       axios.get("/notice")
       .then(res=>{
@@ -27,10 +32,40 @@ export default function Home(){
       setNotice([...notice, inputNotice.current.value]);
       inputNotice.current.value="";
     }
+
+    const [token, setToken] = useState("");
+
+    const getToken = ()=>{
+      //토큰을 발급 받는 요청이기 때문에 토큰 보내지 않기 
+      axios.get("/hello/token",{headers:{Authorization:"none"}})
+      .then(res=>{
+        console.log(res.data);
+        setToken(res.data);
+        //토큰을 localStorage 에 저장하기 (react 와 상관없는 웹브라우저의 저장소)
+        localStorage.token=res.data;
+        //axios 의 요청헤더에 자동으로 토큰이 담기도록 한다.
+        initToken(res.data);
+      })
+      .catch(error=>{
+        console.log(error);
+      })
+    }
+    const request = ()=>{
+      axios.get("/fortune")
+      .then(res=>{
+        console.log(res.data);
+      })
+      .catch(error=>{
+        console.log(error);
+      })
+    }
+
     return (
       <>
         <h1>인덱스 페이지 입니다.</h1>
-        
+        <button onClick={getToken}>토큰 받아오기</button>
+        <button onClick={request}>토큰과 함께 요청하기</button>
+        <p>{token}</p>
         <h3>공지사항</h3>
         <div className="row">
           <div className="col">
